53bb3f304b02b5aa3b11199a4083717d1952a09c
Phili0916 <79313259+Phili0916@users.noreply.github.com>
2023-07-27 11:02:52 +0200

    added videos and new messages

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const menuBtn = document.querySelector(".menu-btn")
 const navigationBtn = document.querySelector(".navigation")
 const sliderBtns = document.querySelectorAll(".slider-btn")
+const slides = document.querySelectorAll(".video-slide")
+const contents = document.querySelectorAll(".content")
+
 
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("active")
@@ -12,7 +15,15 @@ const sliderBtnNavigation = function(manual) {
     sliderBtns.forEach((sliderBtn) => {
         sliderBtn.classList.remove("active")
     })
+    slides.forEach((slide) => {
+        slide.classList.remove("active")
+    })
+    contents.forEach((content) => {
+        content.classList.remove("active")
+    })
     sliderBtns[manual].classList.add("active")
+    slides[manual].classList.add("active")
+    contents[manual].classList.add("active")
 }
 
 sliderBtns.forEach((sliderBtn, i) => {
